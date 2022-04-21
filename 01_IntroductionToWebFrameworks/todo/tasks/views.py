731554b3543926efbff8ba52c69975887af5dab8
Argunov Fedor <fargunov@internet.ru>
2022-04-21 04:09:54 +0300

    Модуль 2 Вывести список задач случайным образом

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,14 +1,16 @@
 from django.http import HttpResponse
 
 from django.views import View
+import random
 
 
 class ToDoView(View):
-
     def get(self, request, *args, **kwargs):
-        return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
+        tasks = ['<li>Установить python</li>',
+                 '<li>Установить django</li>',
+                 '<li>Запустить сервер</li>',
+                 '<li>Порадоваться результату</li>']
+        return HttpResponse('<ul>' +
+                            ''.join([tasks.pop(random.randrange(len(tasks))) for _ in range(len(tasks))])
+                            +
                             '</ul>')
