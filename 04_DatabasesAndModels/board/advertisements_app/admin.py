3e1bdbb9b151dc1abd7b063d92bcfd9a9822fa22
Аргунов Федор Михайлович <fargunov@internet.ru>
2022-06-23 15:58:52 +0900

    Модуль 7 Добавить основное решение

--- 04_DatabasesAndModels/board/advertisements_app/admin.py
+++ 04_DatabasesAndModels/board/advertisements_app/admin.py
@@ -1,7 +1,48 @@
 from django.contrib import admin
-from advertisements_app.models import Advertisement
+from advertisements_app.models import Advertisement, Author, AdvertisementType, News, Comments
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = Comments
+
+
 @admin.register(Advertisement)
 class AdvertisementAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(AdvertisementType)
+class AdvertisementTypeAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_filter = ['is_active']
+    list_display = ['id', 'name', 'created_at', 'is_active']
+    inlines = [CommentInline]
+    actions = ['mark_as_active', 'mark_as_inactive']
+
+    def mark_as_active(self, request, queryset):
+        queryset.update(is_active=True)
+
+    def mark_as_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'text']
+    list_filter = ['username']
+    actions = ['change_text_to_delete']
+
+
+    def change_text_to_delete(self,request,queryset):
+        queryset.update(text='Удалено администратором')
+
+    change_text_to_delete.short_description = 'Изменить текст на удалено администратором'
